5d4da8920913f408b6db02de2d3dd9eaff4e34bb
Agilesh Sivakumar <agilesh.sivakumar@aciworldwide.com>
2019-04-24 18:16:16 +0530

    location and cookie settings done

--- src/app/angular-element-demo/angular-element-demo.component.ts
+++ src/app/angular-element-demo/angular-element-demo.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.Native
 })
 export class AngularElementDemoComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() { }
-
+  ngOnInit() {
+    if (window.navigator && window.navigator.geolocation) {
+      window.navigator.geolocation.getCurrentPosition(
+        position => {
+          console.log(position);
+        },
+        error => {
+          switch (error.code) {
+            case 1:
+              console.log('Permission Denied');
+              break;
+            case 2:
+              console.log('Position Unavailable');
+              break;
+            case 3:
+              console.log('Timeout');
+              break;
+          }
+        }
+      );
+    }
+  }
 }
